c798b3200932cec7f31dc216c28019c3e13a2e66
nnethercott <nathaniel@deepomatic.com>
2025-05-23 15:04:16 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,60 +1,86 @@
 # arroy benchmarks
-A small repo to test if my features actually improve perfs
+A small repo to test if my features actually improve performance. Benchmarks done with criterion.
 
-vectors from https://meilisearch.notion.site/Movies-embeddings-1de3258859f54b799b7883882219d266
+datacomp small vectors: https://meilisearch.notion.site/Movies-embeddings-1de3258859f54b799b7883882219d266
 
-## vector store benchmark results
-median top k
-```
-Datacomp small - 12799999 vectors of 768 dimensions
-10000 vectors are used for this measure and 18446744073709551615B of memory
-Recall tested is:   [   1,   10,   20,   50,  100,  500]
-Starting indexing process
-indexing: 50.38s, size: 132.02 MiB, indexed in 1x
-[arroy]  Cosine x1: [1.00, 0.81, 0.83, 0.89, 0.92, 0.97], searched for: 6.89s, searched in 100.00%
-[arroy]  Cosine x1: [1.00, 0.84, 0.88, 0.92, 0.95, 0.99], searched for: 6.01s, searched in 50.00%
-[arroy]  Cosine x1: [1.00, 0.89, 0.91, 0.94, 0.96, 0.99], searched for: 6.96s, searched in 25.00%
-[arroy]  Cosine x1: [1.00, 0.92, 0.93, 0.95, 0.96, 1.00], searched for: 8.08s, searched in 15.00%
-[arroy]  Cosine x1: [1.00, 0.93, 0.94, 0.96, 0.97, 1.00], searched for: 9.47s, searched in 10.00%
-[arroy]  Cosine x1: [1.00, 0.94, 0.94, 0.96, 0.97, 1.00], searched for: 9.78s, searched in 8.00%
-[arroy]  Cosine x1: [1.00, 0.94, 0.95, 0.96, 0.97, 1.00], searched for: 11.23s, searched in 6.00%
-[arroy]  Cosine x1: [1.00, 0.96, 0.96, 0.98, 1.00, 0.40], searched for: 14.68s, searched in 2.00%
-[arroy]  Cosine x1: [1.00, 0.97, 0.98, 0.99, 1.00, 0.20], searched for: 17.92s, searched in 1.00%
-[arroy]  Cosine x3: [1.00, 0.88, 0.91, 0.95, 0.96, 1.00], searched for: 12.81s, searched in 100.00%
-[arroy]  Cosine x3: [1.00, 0.92, 0.94, 0.96, 0.97, 1.00], searched for: 11.24s, searched in 50.00%
-[arroy]  Cosine x3: [1.00, 0.94, 0.95, 0.97, 0.98, 1.00], searched for: 15.47s, searched in 25.00%
-[arroy]  Cosine x3: [1.00, 0.95, 0.96, 0.97, 0.99, 1.00], searched for: 26.28s, searched in 15.00%
-[arroy]  Cosine x3: [1.00, 0.96, 0.96, 0.98, 0.99, 1.00], searched for: 27.35s, searched in 10.00%
-[arroy]  Cosine x3: [1.00, 0.96, 0.96, 0.98, 1.00, 1.00], searched for: 25.56s, searched in 8.00%
-[arroy]  Cosine x3: [1.00, 0.96, 0.96, 0.99, 1.00, 1.00], searched for: 27.04s, searched in 6.00%
-[arroy]  Cosine x3: [1.00, 0.98, 0.99, 1.00, 1.00, 0.40], searched for: 27.09s, searched in 2.00%
-[arroy]  Cosine x3: [1.00, 0.99, 1.00, 1.00, 1.00, 0.20], searched for: 29.80s, searched in 1.00%
+# checks
+## binary heap vs median-based top k
+Constructing a binary heap from n items and popping k elements costs O(n + klog(n)). Using a median-based strategy this cost goes down to O(n+klog(k)). Strategy from [this blog post](https://quickwit.io/blog/top-k-complexity).
+
+<details>
+  <summary>Sample bench</summary>
+
+  ```bash
+  theoretical/heap/(n:100,k:10)
+                          time:   [1.3426 µs 1.3540 µs 1.3672 µs]
+  theoretical/median/(n:100,k:10)
+                          time:   [974.39 ns 984.91 ns 997.26 ns]
+  theoretical/heap/(n:100,k:100)
+                          time:   [4.5147 µs 4.5563 µs 4.5997 µs]
+  theoretical/median/(n:100,k:100)
+                          time:   [3.2994 µs 3.3830 µs 3.4817 µs]
+  theoretical/heap/(n:1000,k:10)
+                          time:   [9.5652 µs 9.7025 µs 9.8644 µs]
+  theoretical/median/(n:1000,k:10)
+                          time:   [2.4634 µs 2.4949 µs 2.5329 µs]
+  theoretical/heap/(n:1000,k:100)
+                          time:   [15.354 µs 15.577 µs 15.836 µs]
+  theoretical/median/(n:1000,k:100)
+                          time:   [12.392 µs 12.535 µs 12.714 µs]
+  theoretical/heap/(n:1000,k:1000)
+                          time:   [68.019 µs 68.696 µs 69.489 µs]
+  theoretical/median/(n:1000,k:1000)
+                          time:   [44.650 µs 45.091 µs 45.596 µs]
+  theoretical/heap/(n:10000,k:10)
+                          time:   [87.452 µs 88.466 µs 89.602 µs]
+  theoretical/median/(n:10000,k:10)
+                          time:   [9.0498 µs 9.2777 µs 9.5439 µs]
+  theoretical/heap/(n:10000,k:100)
+                          time:   [98.695 µs 100.90 µs 103.61 µs]
+  theoretical/median/(n:10000,k:100)
+                          time:   [27.534 µs 27.969 µs 28.473 µs]
+  theoretical/heap/(n:10000,k:1000)
+                          time:   [182.67 µs 184.45 µs 186.40 µs]
+  theoretical/median/(n:10000,k:1000)
+                          time:   [131.45 µs 132.71 µs 134.14 µs]
+
+  ```
+
+</details>
+
+## replace OrderedFloat package with byte-wise Ord on u32 trasmutes
+I got this idea while reading [this article](https://ohadravid.github.io/posts/2025-05-rav1d-faster/#replace-field-wise-equality-with-byte-wise-equality-that-optimizes-better).
+
+Essentially since distances are such that d(x,y)>=0 (it's one of their [core properties](https://en.wikipedia.org/wiki/Metric_space#Definition_and_illustration)) we can afford a cheaper comparison between non-negative f32s. As a u32 there's only a few special floats we need to watch out for; 
+* inf which has all exponent bits set to 1's and 0's everywhere
+* nan which has all exponent bits set to 1's and at least one non zero fraction bit set to 1
+
+This means that `f32::INFINITY.to_bits()<f32::NAN.to_bits()` ! But we don't really care since NaN's should be caught earlier before our sorting ops.
+
+The ordered-float package implements `Ord` for floating point types, but when sorting distances (like when trying to find the top k elements) we don't need its full expressiveness - we can just cast the f32 to bits and compare directly. Doing it this way actually yields significant speedups. 
+
+For this benchmark I just timed how long it took to build a binary heap from each wrapper type for varying input sizes.
+
+
+![alt-text](https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Float_example.svg/885px-Float_example.svg.png)
+
+<details>
+  <summary>Sample bench</summary>
+
+  ```
+  OrderedFloat/n=10
+             time:   [68.947 ns 70.406 ns 72.469 ns]
+  NonNegativeOrderedFloat/n=10
+             time:   [50.342 ns 50.838 ns 51.402 ns]
+  OrderedFloat/n=100
+             time:   [861.26 ns 877.75 ns 897.06 ns]
+  NonNegativeOrderedFloat/n=100
+             time:   [445.68 ns 452.93 ns 461.57 ns]
+  OrderedFloat/n=1000
+             time:   [8.9741 µs 9.1250 µs 9.2960 µs]
+  NonNegativeOrderedFloat/n=1000
+             time:   [3.8946 µs 3.9375 µs 3.9892 µs]
 ```
 
-main 
-```
-Datacomp small - 12799999 vectors of 768 dimensions
-10000 vectors are used for this measure and 18446744073709551615B of memory
-Recall tested is:   [   1,   10,   20,   50,  100,  500]
-Starting indexing process
-indexing: 56.79s, size: 132.02 MiB, indexed in 1x
-[arroy]  Cosine x1: [0.95, 0.81, 0.81, 0.87, 0.92, 0.97], searched for: 7.40s, searched in 100.00%
-[arroy]  Cosine x1: [0.95, 0.83, 0.85, 0.90, 0.95, 0.99], searched for: 6.49s, searched in 50.00%
-[arroy]  Cosine x1: [0.95, 0.88, 0.89, 0.92, 0.95, 0.99], searched for: 8.14s, searched in 25.00%
-[arroy]  Cosine x1: [0.95, 0.91, 0.90, 0.93, 0.96, 1.00], searched for: 9.23s, searched in 15.00%
-[arroy]  Cosine x1: [0.95, 0.92, 0.91, 0.93, 0.96, 1.00], searched for: 10.03s, searched in 10.00%
-[arroy]  Cosine x1: [0.95, 0.93, 0.91, 0.93, 0.96, 1.00], searched for: 12.47s, searched in 8.00%
-[arroy]  Cosine x1: [0.95, 0.93, 0.92, 0.94, 0.97, 1.00], searched for: 13.33s, searched in 6.00%
-[arroy]  Cosine x1: [0.95, 0.95, 0.93, 0.96, 0.99, 0.40], searched for: 17.59s, searched in 2.00%
-[arroy]  Cosine x1: [0.95, 0.96, 0.95, 0.97, 1.00, 0.20], searched for: 19.84s, searched in 1.00%
-[arroy]  Cosine x3: [0.95, 0.87, 0.88, 0.92, 0.96, 1.00], searched for: 12.92s, searched in 100.00%
-[arroy]  Cosine x3: [0.95, 0.91, 0.91, 0.94, 0.97, 1.00], searched for: 11.50s, searched in 50.00%
-[arroy]  Cosine x3: [0.95, 0.93, 0.92, 0.94, 0.98, 1.00], searched for: 16.68s, searched in 25.00%
-[arroy]  Cosine x3: [0.95, 0.94, 0.93, 0.95, 0.98, 1.00], searched for: 27.62s, searched in 15.00%
-[arroy]  Cosine x3: [0.95, 0.95, 0.93, 0.95, 0.99, 1.00], searched for: 27.65s, searched in 10.00%
-[arroy]  Cosine x3: [0.95, 0.95, 0.93, 0.96, 0.99, 1.00], searched for: 28.35s, searched in 8.00%
-[arroy]  Cosine x3: [0.95, 0.95, 0.94, 0.96, 0.99, 1.00], searched for: 27.58s, searched in 6.00%
-[arroy]  Cosine x3: [0.95, 0.97, 0.96, 0.97, 1.00, 0.40], searched for: 33.41s, searched in 2.00%
-[arroy]  Cosine x3: [0.95, 0.98, 0.97, 0.97, 1.00, 0.20], searched for: 32.48s, searched in 1.00%
-```
+</details>
 
--- benches/reader.rs
+++ benches/reader.rs
@@ -1,35 +1,26 @@
-use arroy::{Database, ItemId, QueryBuilder, Reader, Writer, distances::Cosine};
+use arroy::{Database, Reader, distances::Cosine};
 use arroy_benchmarks::custom_ordered_float::NonNegativeOrderedFloat;
 use core::f32;
 use criterion::{BatchSize, BenchmarkId, Criterion, criterion_group, criterion_main};
-use heed::{Env, EnvOpenOptions, RoTxn, WithTls};
+use heed::EnvOpenOptions;
 use ordered_float::OrderedFloat;
-use rand::{
-    distributions::Uniform,
-    prelude::*,
-    rngs::{self, OsRng},
-};
+use rand::{distributions::Uniform, prelude::*, rngs::OsRng};
 use std::{
-    cell::Cell,
+    // cell::Cell,
     cmp::Reverse,
     collections::BinaryHeap,
-    fs,
     hint::black_box,
-    num::NonZeroUsize,
-    time::{Duration, Instant},
+    time::Duration,
 };
 
-const N: usize = 100;
+//  sed "2q;d" vectors.txt | awk '{for (i=2; i<=NF; i++) print $i}' | wc -l
+
 const DEFAULT_MAP_SIZE: usize = 1024 * 1024 * 1024 * 2;
 
-//  sed "2q;d" vectors.txt | awk '{for (i=2; i<=NF; i++) print $i}' | wc -l
-const DIMENSIONS: usize = 768;
-
-// some helpers simulating the type of data in reader.nns(k).by_item()
-fn data_gen() -> Vec<(OrderedFloat<f32>, u32)> {
+// some data
+fn data_gen(n: usize) -> Vec<(OrderedFloat<f32>, u32)> {
     let uniform = Uniform::new(0.0f32, 1.0);
-
-    (0..N)
+    (0..n)
         .map(|i| {
             let dist = uniform.sample(&mut OsRng);
             (OrderedFloat(dist), i as u32)
@@ -37,11 +28,10 @@ fn data_gen() -> Vec<(OrderedFloat<f32>, u32)> {
         .collect()
 }
 
-// need Reverse for min binary heap
-fn data_gen_rev() -> Vec<Reverse<(OrderedFloat<f32>, u32)>> {
+// some reversed data !
+fn data_gen_rev(n: usize) -> Vec<Reverse<(OrderedFloat<f32>, u32)>> {
     let uniform = Uniform::new(0.0f32, 1.0);
-
-    (0..N)
+    (0..n)
         .map(|i| {
             let dist = uniform.sample(&mut OsRng);
             Reverse((OrderedFloat(dist), i as u32))
@@ -49,103 +39,69 @@ fn data_gen_rev() -> Vec<Reverse<(OrderedFloat<f32>, u32)>> {
         .collect()
 }
 
-/// bench time for building binary heap from f32 wrappers
-fn race_ordered_floats(c: &mut Criterion) {
-    let mut group = c.benchmark_group("ord f32 race");
-
-    group.bench_function(BenchmarkId::from_parameter("Ordered"), |b| {
-        b.iter_batched(
-            data_gen,
-            |v| {
-                BinaryHeap::from(v);
-            },
-            BatchSize::LargeInput,
-        );
-    });
-
-    group.bench_function(
-        BenchmarkId::from_parameter("NonNegativeOrderedFloat"),
-        |b| {
-            b.iter_batched(
-                || {
-                    let uniform = Uniform::new(0.0f32, 1.0);
-
-                    (0..N)
-                        .map(|i| {
-                            let dist = uniform.sample(&mut OsRng);
-                            (NonNegativeOrderedFloat(dist), i as u32)
-                        })
-                        .collect::<Vec<(NonNegativeOrderedFloat, u32)>>()
-                },
-                |v| {
-                    BinaryHeap::from(v);
-                },
-                BatchSize::LargeInput,
-            );
-        },
-    );
-}
-
 /// bench different top k strategies
 fn theoretical_top_k(c: &mut Criterion) {
     let mut group = c.benchmark_group("theoretical");
 
-    for k in vec![10, 100, 1000] {
-        group.bench_function(BenchmarkId::new("heap: O(n+k*log(n))", k), move |b| {
-            b.iter_batched(
-                data_gen_rev,
-                |v| {
-                    // Here we're inserting n items items into a binary heap
-                    let mut min_heap = BinaryHeap::from(v);
-                    let mut output = Vec::with_capacity(k);
+    for n in [100, 1000, 10000] {
+        'inner: for k in vec![10, 100, 1000] {
+            if k>n{
+                break 'inner;
+            }
+            group.bench_function(BenchmarkId::new("heap", format!("(n:{},k:{})", n, k)), move |b| {
+                b.iter_batched(
+                    || data_gen_rev(n),
+                    |v| {
+                        let mut min_heap = BinaryHeap::from(v);
+                        let mut output = Vec::with_capacity(k);
 
-                    while let Some(Reverse((OrderedFloat(dist), item))) = min_heap.pop() {
-                        if output.len() == k {
-                            break;
+                        while let Some(Reverse((OrderedFloat(_dist), item))) = min_heap.pop() {
+                            if output.len() == k {
+                                break;
+                            }
+                            output.push((item, 0.0f32));
                         }
-                        output.push((item, 0.0f32));
-                    }
-                },
-                criterion::BatchSize::LargeInput,
-            );
-        });
+                    },
+                    criterion::BatchSize::LargeInput,
+                );
+            });
 
-        group.bench_function(BenchmarkId::new("median: O(n+k*log(k))", k), move |b| {
-            b.iter_batched(
-                data_gen,
-                |mut v| {
-                    let mut buffer = Vec::with_capacity(2 * k);
-                    let mut v = v.into_iter();
-                    buffer.extend((&mut v).take(k));
-                    let mut threshold = OrderedFloat(f32::MAX);
+            group.bench_function(BenchmarkId::new("median", format!("(n:{},k:{})", n, k)), move |b| {
+                b.iter_batched(
+                    || data_gen(n),
+                    |v| {
+                        let mut buffer = Vec::with_capacity(2 * k);
+                        let mut v = v.into_iter();
+                        buffer.extend((&mut v).take(k));
+                        let mut threshold = OrderedFloat(f32::MAX);
 
-                    for item in v {
-                        if item.0 >= threshold {
-                            continue;
+                        for item in v {
+                            if item.0 >= threshold {
+                                continue;
+                            }
+                            if buffer.len() == 2 * k {
+                                let (_, &mut median, _) = buffer.select_nth_unstable(k - 1);
+                                threshold = median.0;
+                                buffer.truncate(k);
+                            }
+
+                            // buffer resizing here?
+                            // buffer.push(item);
+                            let uninit = buffer.spare_capacity_mut();
+                            uninit[0].write(item);
+                            unsafe {
+                                buffer.set_len(buffer.len() + 1);
+                            }
                         }
-                        if buffer.len() == 2 * k {
-                            let (_, &mut median, _) = buffer.select_nth_unstable(k - 1);
-                            threshold = median.0;
-                            buffer.truncate(k);
-                        }
 
-                        // buffer resizing here?
-                        // buffer.push(item);
-                        let uninit = buffer.spare_capacity_mut();
-                        uninit[0].write(item);
-                        unsafe {
-                            buffer.set_len(buffer.len() + 1);
-                        }
-                    }
-
-                    // final sort
-                    buffer.sort_unstable();
-                    buffer.truncate(k);
-                },
-                criterion::BatchSize::LargeInput,
-            );
-        });
-
+                        // final sort
+                        buffer.sort_unstable();
+                        buffer.truncate(k);
+                    },
+                    criterion::BatchSize::LargeInput,
+                );
+            });
+        }
         // group.bench_function(BenchmarkId::new("O(n+k*log(n)*log(k))", k), move |b| {
         //     b.iter_batched(
         //         || data_gen().into_iter(),
@@ -178,13 +134,52 @@ fn theoretical_top_k(c: &mut Criterion) {
     }
 }
 
+/// bench time for building binary heap from f32 wrappers
+#[allow(dead_code)]
+fn race_ordered_floats(c: &mut Criterion) {
+    let mut group = c.benchmark_group("ord f32 race");
+
+    for n in vec![10, 100, 1000] {
+        group.bench_function(BenchmarkId::new("OrderedFloat", n), |b| {
+            b.iter_batched(
+                || data_gen(n),
+                |v| {
+                    let _ = BinaryHeap::from(v);
+                },
+                BatchSize::LargeInput,
+            );
+        });
+
+        group.bench_function(BenchmarkId::new("NonNegativeOrderedFloat", n), |b| {
+            b.iter_batched(
+                // kinda wasteful but hey
+                || {
+                    let uniform = Uniform::new(0.0f32, 1.0);
+                    (0..n)
+                        .map(|i| {
+                            let dist = uniform.sample(&mut OsRng);
+                            (NonNegativeOrderedFloat(dist), i as u32)
+                        })
+                        .collect::<Vec<(NonNegativeOrderedFloat, u32)>>()
+                },
+                |v| {
+                    let _ = BinaryHeap::from(v);
+                },
+                BatchSize::LargeInput,
+            );
+        });
+    }
+}
+
 /// bench proxy for real code perf
+#[allow(dead_code)]
 fn reader_by_item(c: &mut Criterion) -> arroy::Result<()> {
     let mut group = c.benchmark_group("arroy");
-    // group.sample_size(20000);
-    group.measurement_time(Duration::from_secs(30)).sample_size(1000);
+    group
+        .measurement_time(Duration::from_secs(60))
+        .sample_size(1000);
 
-    // setup
+    // setup; 20 trees, 5k vectors, 768 dimensions
     let mut env_builder = EnvOpenOptions::new();
     env_builder.map_size(DEFAULT_MAP_SIZE);
     let env = unsafe { env_builder.open("assets/import.ary/") }.unwrap();
@@ -192,22 +187,20 @@ fn reader_by_item(c: &mut Criterion) -> arroy::Result<()> {
     let database: Database<Cosine> = env.open_database(&rtxn, None)?.unwrap();
     let reader = Reader::open(&rtxn, 0, database)?;
 
-    // deterministic sampling
-    // let mut rng = StdRng::seed_from_u64(42);
-    let n_items = reader.n_items() as u32;
-    let counter = Cell::new(0);
+    // let n_items = reader.n_items() as u32;
+    // let counter = Cell::new(0);
 
-    for nns in vec![10, 100, 1000] {
+    for nns in vec![100] {
         group.bench_function(BenchmarkId::new("reader", nns), |b| {
             b.iter_batched(
                 || {
-                    let item = counter.get();
-                    counter.set((item + 1) % n_items);
-                    // let item: u32 = rng.gen_range(0..n_items);
-                    (reader.nns(nns), item)
+                    // let item = counter.get();
+                    // counter.set((item + 1) % n_items);
+                    // (reader.nns(nns), item)
+                    reader.nns(nns)
                 },
-                |(mut builder, index)| {
-                    black_box(builder.by_item(&rtxn, index));
+                |builder| {
+                    let _ = black_box(builder.by_item(&rtxn, 42));
                 },
                 criterion::BatchSize::SmallInput,
             );
@@ -219,8 +212,8 @@ fn reader_by_item(c: &mut Criterion) -> arroy::Result<()> {
 
 criterion_group!(
     benches,
-    // race_ordered_floats,
     // theoretical_top_k,
-    reader_by_item,
+    race_ordered_floats,
+    // reader_by_item,
 );
 criterion_main!(benches);
